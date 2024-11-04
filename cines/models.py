d8eca9ef0c105ed7aeeffd8c32ce6ec4179c4b25
NoeVillaTorres <noevdm2@hotmail.com>
2024-11-03 22:37:03 -0600

    avances para el login

--- cines/models.py
+++ cines/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from usuarios_cine.models import Usuario
 
-# Create your models here.
+class Cine(models.Model):
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=200)
+    capacidad = models.IntegerField()
+    propietario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name="cines")
+
+    def __str__(self):
+        return self.nombre
--- usuarios_cine/urls.py
+++ usuarios_cine/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import home, predicciones_page
 
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include(usuarios_cine.urls)),
+    path("", home, name='home'),
+    path("", predicciones_page, name='predicciones_page'),
 ]
